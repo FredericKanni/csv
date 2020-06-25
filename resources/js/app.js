d627085d7b1c5dc6cfbda1cf98447c9106eb25cf
FREDERIC <kannif@ymail.com>
2020-06-25 14:21:03 +0400

    import de vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,18 @@ require('./bootstrap');
 
 import Vue from 'vue';
 
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
 window.Vue = require('vue');
+Vue.use(Vuetify);
 
 import Csv from './importCSV.vue';
 Vue.component('Csv', Csv);
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    vuetify: new Vuetify({}),
 });
+
+export default new Vuetify(app);
